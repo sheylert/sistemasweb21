8795778e008dac931e62a7430e92bb01534ab560
sheylert <sheylert@gmail.com>
2018-02-15 20:40:54 -0400

    arreglo de inserts en la bd

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 var Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("pronota_20000","postgres","123456", {  
+const sequelize = new Sequelize("pronota_prueba2","postgres","123456", {   
 dialect: 'postgres'
 });
 
 
 const models = {
-	User: sequelize.import('./user'),
-	Client: sequelize.import('./client'),
-	Responsable: sequelize.import('./responsable'),
 	Profile: sequelize.import('./profile'),
+	Client: sequelize.import('./client'),
+	User: sequelize.import('./user'),
+	Responsable: sequelize.import('./responsable'),
 	Course: sequelize.import('./course'),
 	Teacher: sequelize.import('./teacher'),
 	Subject: sequelize.import('./subject'),
